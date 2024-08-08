f233b5b44d2fe520b406e1e278d40bb18bfac2e9
the2roock <maksympetrynko68@gmail.com>
2024-08-08 12:41:13 +0300

    drop Price from features

--- model.py
+++ model.py
@@ -5,14 +5,12 @@ import torch.nn.functional as F
 class VehiclePriceIdentifier(nn.Module):
     def __init__(self):
         super().__init__()
-        self.fc1 = nn.Linear(in_features=12, out_features=16)
-        self.fc2 = nn.Linear(in_features=16, out_features=8)
-        self.fc3 = nn.Linear(in_features=8, out_features=1)
+        self.fc1 = nn.Linear(in_features=11, out_features=8)
+        self.fc2 = nn.Linear(in_features=8, out_features=1)
     
     def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        out = self.fc3(x)
+        x = F.sigmoid(self.fc1(x))
+        out = self.fc2(x)
         return out
     
     @property
